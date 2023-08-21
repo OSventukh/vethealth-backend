bb4b536ee5cb8b156893777ab2f4dc96522e3ebf
Oleksandr Sventukh <o.sventukh@gmail.com>
2023-08-21 19:39:53 +0300

    feat: configure database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import databaseConfig from './config/database.config';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+      dataSourceFactory: async (options: DataSourceOptions) => {
+        console.log(options)
+        return new DataSource(options).initialize();
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
