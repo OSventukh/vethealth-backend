1bf62ada077dbde1a8004be85f2a016e62efcad2
Oleksandr Sventukh <o.sventukh@gmail.com>
2023-12-11 13:09:16 +0200

    feat: create combobox element

--- src/topics/entities/topic.entity.ts
+++ src/topics/entities/topic.entity.ts
@@ -43,7 +43,6 @@ export class TopicEntity {
   description?: string;
 
   @ManyToOne(() => TopicStatusEntity, { eager: true })
-  @Transform(({ obj }) => obj?.status?.name)
   status: TopicStatusEntity;
 
   @Column({ unique: true })
